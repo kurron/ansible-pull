209956a8844561ee01b09b018096b78da51a1944
Ron Kurr <kurr@kurron.org>
2015-09-25 01:15:52 +0000

    Upgraded to the current Docker and removed installation of containers

--- scripts/download-jdk-8.sh
+++ scripts/download-jdk-8.sh
@@ -7,7 +7,7 @@ if [ -f "${DONEFILE}" ]; then
     exit 0
 fi
 
-wget --quiet --output-document=/root/jdk-8.tar.gz --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jdk-8u51-linux-x64.tar.gz 
+wget --quiet --output-document=/root/jdk-8.tar.gz --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.tar.gz 
 
 # signal a successful provision
 touch ${DONEFILE}
